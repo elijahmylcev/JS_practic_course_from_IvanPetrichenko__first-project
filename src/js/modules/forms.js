e97934e61c0bdc8c8a08b8736da0547f0af055e5
Elijah <elijahmylcev@gmail.com>
2021-11-27 20:46:18 +0300

    write express server

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,6 +1,8 @@
+import { post } from 'jquery';
+
 const forms = () => {
   const form = document.querySelectorAll('form');
-  const input = document.querySelectorAll('input');
+  const inputs = document.querySelectorAll('input');
 
   const message = {
     loading: 'Загрузка...',
@@ -8,11 +10,19 @@ const forms = () => {
     failure: 'Что-то пошло не так...',
   };
 
-  const postData = (url, data) => {
+  const postData = async (url, data) => {
     document.querySelector('.status').textContent = message.loading;
     let res = await fetch(url, {
       method: 'POST',
       body: data,
+    });
+
+    return await res.text();
+  };
+
+  const clearInputs = () => {
+    inputs.forEach((item) => {
+      item.value = '';
     });
   };
 
@@ -24,6 +34,19 @@ const forms = () => {
       item.appendChild(statusMessage);
 
       const formData = new FormData(item);
+
+      postData('assets/server.php', formData)
+        .then((res) => {
+          console.log(res);
+          statusMessage.textContent = message.success;
+        })
+        .catch(() => (statusMessage.textContent = message.failure))
+        .finally(() => {
+          clearInputs();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 5000);
+        });
     });
   });
 };
